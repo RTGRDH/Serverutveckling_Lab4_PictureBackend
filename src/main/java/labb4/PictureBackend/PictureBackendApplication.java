b237f7b6741b6460b9f9cdd778f561efb67daaaa
carl-bernhardhallberg <cbha@kth.se>
2021-01-05 18:57:48 +0100

    - Init

---
+++ src/main/java/labb4/PictureBackend/PictureBackendApplication.java
@@ -0,0 +1,13 @@
+package labb4.PictureBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/labb4/PictureBackend/PictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package labb4.PictureBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
